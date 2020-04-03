f35e5264dfb0b3832b45f7c46872c4ebd2c3ec3a
khlms <17987841+khlms@users.noreply.github.com>
2020-04-03 11:22:59 +0200

    add: download patent pictures using urllib

--- patent-google-scraper.py
+++ patent-google-scraper.py
@@ -4,11 +4,12 @@ import sys
 import requests
 from bs4 import BeautifulSoup
 # import csv
-import pandas
+# import pandas
+from urllib.request import urlretrieve
+from urllib.error import HTTPError
 
 
-
-patent = input("Enter the patent number: ")
+patent = input("Enter the patent number: ").strip()
 
 if patent == '':
     print("no patent entered")
@@ -25,8 +26,35 @@ else:
     title = PatentSoup.find("meta", {"name":"DC.title"}).get("content")
     ## alternative:
     # title = PatentSoup.find("span", {"itemprop": "title"}).get_text(strip=True)
-    claims = [PatentSoup.find("div", {"class":"claim", "num":"00001"}).get_text(strip=True)] + [claim.get_text(strip=True) for claim in PatentSoup.find_all("div", {"class": "claim-dependent"})]
-    df = pandas.DataFrame(claims)
-    df.to_csv("patents.csv", index=False, header=False)
+    #
+    ## get list of claims
+    # claims = [PatentSoup.find("div", {"class":"claim", "num":"00001"})] + [claim for claim in PatentSoup.find_all("div", {"class": "claim-dependent"})]
+    # claims = [PatentSoup.find("div", {"class":"claim", "num":"00001"})] + PatentSoup.find_all("div", {"class": "claim-dependent"})
+    #
+    ## csv-output using csv:
+    # str_claims = ','.join(claim.get_text(strip=False).strip() for claim in claims)
+    # print(str_claims)
+    # with open ('patents.csv', 'w') as csvfile:
+        # writer = csv.writer(csvfile, delimiter=",")
+        # writer.writerow(str_claims)
+    #
+    ## csv-output using pandas
+    # ClaimsCSV = [claim.get_text(strip=False).strip() for claim in claims]
+    # df = pandas.DataFrame(ClaimsCSV)
+    # df.to_csv("patents.csv", index=False, header=False)
+    # get all claims
+    claims = PatentSoup.find_all("div",{"class":"claims"})
+    #
+    try:
+        PicturesAll = PatentSoup.find_all("li",{"itemprop":"images"})
+        PicturesURLs = [picture.find("meta",{"itemprop":"full"}).get("content") for picture in PicturesAll]
+        for picture in PicturesURLs:
+            urlretrieve(picture, picture.split('/')[-1])
+    except FileNotFoundError as err:
+        print(err)
+    except HTTPError as err:
+        print(err)
     print(title)
     print(claims)
+    print(PicturesAll)
+    print(PicturesURLs)
